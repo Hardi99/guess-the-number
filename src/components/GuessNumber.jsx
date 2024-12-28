57558d0f00135a2aadf4d460f4a02fc1e36b03f1
Hardi99 <harditabuna@gmail.com>
2024-12-28 08:38:48 +0100

    big updates UX

--- src/components/GuessNumber.jsx
+++ src/components/GuessNumber.jsx
@@ -9,6 +9,7 @@ function GuessTheNumber() {
     const [message2, setMessage2] = useState('')
     const [attempts, setAttempts] = useState([])
     const [secretNumber, setSecretNumber] = useState(Math.floor(Math.random() * 100));
+    const [hasWon, setHasWon] = useState(false); // État pour suivre si le joueur a gagné
 
     const handleChange = (event) => {
         setGuess(event.target.value);
@@ -23,12 +24,13 @@ function GuessTheNumber() {
         }
 
         if (guess < secretNumber) {
-            setMessage('Trop bas!');
+            setMessage('Trop petit !');
         } else if (guess > secretNumber) {
-            setMessage('Trop haut!');
+            setMessage('Trop grand !');
         } else {
-            setMessage('Bravo ! Vous avez deviné le nombre !');
-            setMessage2(`Vous avez trouvé en ${count} tentatives en essayant précédemment [${attempts.join(', ')}]`);
+            setMessage('Bravo vous avez deviné le nombre !');
+            setMessage2(`Vous avez trouvé en ${count} tentatives en essayant précédemment [${attempts}]`);
+            setHasWon(true); // Mettre à jour l'état hasWon lorsque le nombre est trouvé
         }
 
         setAttempts([...attempts, guess]);
@@ -42,6 +44,7 @@ function GuessTheNumber() {
         setAttempts([]);
         setCount(1);
         setSecretNumber(Math.floor(Math.random() * 100));
+        setHasWon(false); // Réinitialiser hasWon lors de la réinitialisation
     };
 
     return (
@@ -49,9 +52,9 @@ function GuessTheNumber() {
                 <h1>🔢 Guess the Number 🔢</h1>
                 <form onSubmit={handleSubmit}>
                     <p>Veuillez saisir un chiffre</p>
-                    <input type="number" min={0} max={99} step={1} value={guess} onChange={handleChange} />
-                    <button type="submit">Deviner</button>
-                    <button type="reset" onClick={handleReset}>Réinitialiser</button>
+                    <input type="number" min={0} max={99} step={1} value={guess} onChange={handleChange} disabled={hasWon} />
+                    <button type="submit" disabled={hasWon}>Deviner</button>
+                    <button type="reset" onClick={handleReset} disabled={!hasWon}>Réinitialiser</button>
                 </form>
                 <p>{message}</p>
                 <p>{message2}</p>
